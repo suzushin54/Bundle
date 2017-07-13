65b04904b246a22c807c1ce79a25c0a25562d599
G01\suzuki_s <suzuki_s@jp.fujitsu.com>
2017-07-13 17:11:26 +0900

    init commit

---
+++ Bundle/Grep.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Bundle
+{
+    class Grep
+    {
+        public string Extension { get; set; }
+        public string ProgFileEncode { get; set; }
+        public bool IgnoreBinary { get; set; }
+        public string OutputDir { get; set; }
+        public string RegexOptions { get; set; }
+
+        public Grep()
+        {
+            Extension = ConfigurationManager.AppSettings["targetExtension"];
+            ProgFileEncode = ConfigurationManager.AppSettings["programFileEncode"];
+            IgnoreBinary = Boolean.Parse(ConfigurationManager.AppSettings["ignoreBinary"]);
+            OutputDir = ConfigurationManager.AppSettings["outputDir"];
+            RegexOptions = ConfigurationManager.AppSettings["regexOptions"];
+        }
+
+        /// <summary>
+        /// 検索を実行します。
+        /// </summary>
+        /// <param name="word">検索キーワード</param> 
+        /// <param name="fileList">検索対象ファイルリスト</param>
+        /// <param name="subDir">AppConfigから読み込んだKeywordListの種類（Java等）</param>
+        public bool doGrep(string word, string[] fileList, string subDir)
+        {
+            // ここで使用しているRegexOptionsはAppConfigに設定され、Programクラスで読み込んでいます
+            var reg = new Regex(word, (RegexOptions)Enum.Parse(typeof(RegexOptions), RegexOptions));
+            var sb = new StringBuilder();
+
+            try
+            {
+                foreach (string f in fileList)
+                {
+                    // 設定のignoreBinaryがTrueなら、バイナリファイルは検索しない
+                    if (IgnoreBinary && IsBinaryFile(f))
+                            continue;
+
+                    // すべての行を読み込んで、マッチする行を選択する
+                    var result = File.ReadAllLines(@f, Encoding.GetEncoding(ProgFileEncode))
+                               .Select((s, i) => new { Index = i, Value = s })
+                               .Where(s => reg.IsMatch(s.Value));
+
+                    // マッチした行だけをStringBuilderで結合していく
+                    foreach (var r in result)
+                        sb.AppendLine(f + " | " + (r.Index + 1) + "：" + r.Value);
+                }
+
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                throw new DirectoryNotFoundException ("ディレクトリが見つかりません。", e);
+            }
+            catch (System.Exception e)
+            {
+                // あまりよろしくないが全部拾うの面倒なので
+                throw new System.Exception("エラーが発生しました。", e);
+            }
+
+            // マッチした全ての行を、キーワードをファイル名としてテキストに書き出す
+            File.WriteAllText((OutputDir + "\\" + subDir) + "\\" + ValidFileName(word) + ".txt", sb.ToString());
+            sb.Clear();
+
+            return true;
+        }
+
+        /// <summary>
+        /// ファイル名として無効な文字を「_」に置き換える
+        /// ref: http://www.atmarkit.co.jp/fdotnet/dotnettips/551invalidchars/invalidchars.html
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string ValidFileName(string s)
+        {
+            string valid = s;
+            char[] invalidch = Path.GetInvalidFileNameChars();
+
+            foreach (char c in invalidch)
+            {
+                valid = valid.Replace(c, '_');
+            }
+            return valid;
+        }
+
+
+        /// <summary>
+        /// ファイルはバイナリファイルであるかどうか
+        /// ref: http://yellow.ribbon.to/~tuotehhou/index.php?%2BC＃%2Bバイナリ・テキストファイルの判断
+        /// </summary>
+        /// <param name="filePath">パス</param>
+        /// <returns>バイナリファイルの場合trueを返す</returns>
+        public bool IsBinaryFile(string filePath)
+        {
+            FileStream fs = File.OpenRead(filePath);
+            int len = (int)fs.Length;
+            int count = 0;
+            byte[] content = new byte[len];
+            int size = fs.Read(content, 0, len);
+
+            for (int i = 0; i < size; i++)
+            {
+                if (content[i] == 0)
+                {
+                    count++;
+                    if (count == 4)
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    count = 0;
+                }
+            }
+            return false;
+        }
+        
+    }
+}
---
+++ Bundle/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bundle
+{
+    /// <summary>
+    /// 指定したディレクトリに対して検索を行います。
+    /// テキストエディタの検索と異なり、キーワードをリスト形式で複数指定できます。
+    /// キーワードは正規表現で記述してください。
+    /// 結果ファイルはキーワードごとにテキストファイルで出力します。
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Entrypoint
+        /// </summary>
+        /// <param name="args">使用しません</param>
+        static void Main(string[] args)
+        {
+            // 出力先フォルダ
+            string output = ConfigurationManager.AppSettings["outputDir"];
+
+            // 多段で検索するか、設定から取得して判断する
+            int mFlg = Int16.Parse(ConfigurationManager.AppSettings["multipleFlg"]);
+            if(mFlg == 1)
+            {
+                var psr = PrimarySearch(output);
+            } else if(mFlg == 2)
+            {
+                var psr = PrimarySearch(output);
+                var ssr = SecondarySearch(output);
+            } else
+            {
+                // テスト用
+                var ssr = SecondarySearch(output);
+            }
+
+        }
+
+        /// <summary>
+        /// 第一段階の検索を実行する。
+        /// </summary>
+        /// <returns></returns>
+        private static  bool PrimarySearch(string output)
+        {
+            // AppConfigから設定値を読み込む
+            var dic = new Dictionary<string, string>(); // key-value
+            dic.Add("Java", ConfigurationManager.AppSettings["keywordList_java"]);
+            dic.Add("Interstage", ConfigurationManager.AppSettings["keywordList_interstage"]);
+            dic.Add("DB", ConfigurationManager.AppSettings["keywordList_db"]);
+            
+            // 対象フォルダを元に検索対象ファイル一覧を作成する
+            string[] fileList = Directory.GetFiles(ConfigurationManager.AppSettings["targetDir"], 
+                ConfigurationManager.AppSettings["targetExtension"], SearchOption.AllDirectories);
+
+            // 出力予定のフォルダが無かったら作成する
+            if (!Directory.Exists(output))
+                Directory.CreateDirectory(output);
+            
+            string keyword = "";
+            var g = new Grep();
+
+            // AppConfigから読み込んだKeywordListの数だけループする
+            foreach (KeyValuePair<string, string> p in dic)
+            {
+                // 出力予定のサブフォルダが無かったら作成する
+                if (!Directory.Exists(output + "\\" + p.Key))
+                    Directory.CreateDirectory(output + "\\" + p.Key);
+
+                try
+                {
+                    using (var sr = new StreamReader(p.Value, Encoding.GetEncoding(ConfigurationManager.AppSettings["keywordListEncode"])))
+                    {
+                        // キーワードリストの行数分だけ繰り返す
+                        while ((keyword = sr.ReadLine()) != null)
+                        {
+                            // 先頭が# の場合はコメント行として無視する
+                            if ("#".Equals(keyword.Substring(0, 1)))
+                                continue;
+
+                            bool isSuccess = g.doGrep(keyword, fileList, p.Key);
+                            // 処理に失敗した場合は抜ける
+                            if (!isSuccess)
+                            {
+                                Console.WriteLine("検索に失敗しました。");
+                                break;
+                            }
+                        }
+                    }
+                }
+                catch (FileNotFoundException fe)
+                {
+                    Console.WriteLine("【エラー】ファイル: " + p.Value + " が見つかりませんでした。");
+                    Console.WriteLine(p.Key + " は検索されません。");
+                }
+            }
+
+            return true;
+        }
+
+
+        /// <summary>
+        /// 第二段階の検索を実行する。
+        /// </summary>
+        /// <returns></returns>
+        private static bool SecondarySearch(string output)
+        {
+            // 出力予定のサブフォルダが無かったら作成する
+            if (!Directory.Exists(output + "\\Dig"))
+                Directory.CreateDirectory(output + "\\Dig");
+
+            // Digフォルダを対象に、追加Grep用のファイルを検索する
+            foreach (var f in Directory.GetFiles(ConfigurationManager.AppSettings["inputDig"], "*.txt", SearchOption.AllDirectories))
+            {
+                var pathList = new List<string>();
+
+                // 取得したファイル名を元に、Grep結果ファイルを取得する
+                // サブディレクトリが特定できないため、OutputDir+FileNameだけではPathが確定しないので検索している
+                foreach (var outputFile in Directory.GetFiles(output, System.IO.Path.GetFileName(f), SearchOption.AllDirectories))
+                {
+                    // ファイルを繰り返し読み込み、Path部分のみ取得する
+                    foreach (var line in File.ReadLines(outputFile))
+                    {
+                        pathList.Add(line.Substring(0, line.IndexOf("|") - 1));
+                    }
+                    // 検索対象のファイルリストなので、重複は削除する
+                    pathList = pathList.Distinct().ToList();
+                }
+
+                string keyword = "";
+                var g = new Grep();
+                // 作成した検索対象ファイルリストをGrep実行メソッドに渡して実行
+                try
+                {
+                    using (var sr = new StreamReader(f, Encoding.GetEncoding(ConfigurationManager.AppSettings["keywordListEncode"])))
+                    {
+                        // キーワードリストの行数分だけ繰り返す
+                        while ((keyword = sr.ReadLine()) != null)
+                        {
+                            // 先頭が# の場合はコメント行として無視する
+                            if ("#".Equals(keyword.Substring(0, 1)))
+                                continue;
+
+                            bool isSuccess = g.doGrep(keyword, pathList.ToArray(), "Dig");
+                            // 処理に失敗した場合は抜ける
+                            if (!isSuccess)
+                            {
+                                Console.WriteLine("検索に失敗しました。");
+                                break;
+                            }
+                        }
+                    }
+                }
+                catch (FileNotFoundException fe)
+                {
+                    Console.WriteLine("【エラー】ファイル: " + f + " が見つかりませんでした。");
+                }
+
+            }
+
+            return true;
+        }
+     }
+}
